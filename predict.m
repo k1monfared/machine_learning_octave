21174d3f0e5ea64122764ef2991b172382a46f99
Keivan Hassani Monfared <k1monfared@users.noreply.github.com>
2018-10-17 15:42:39 -0600

    w3 upload

--- predict.m
+++ predict.m
@@ -1,18 +1,25 @@
-function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
+function p = predict(theta, X)
+%PREDICT Predict whether the label is 0 or 1 using learned logistic 
+%regression parameters theta
+%   p = PREDICT(theta, X) computes the predictions for X using a 
+%   threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+m = size(X, 1); % Number of training examples
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+% You need to return the following variables correctly
+p = zeros(m, 1);
 
-h1 = sigmoid([ones(m, 1) X] * Theta1');
-h2 = sigmoid([ones(m, 1) h1] * Theta2');
-[dummy, p] = max(h2, [], 2);
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters. 
+%               You should set p to a vector of 0's and 1's
+%
+h = sigmoid(X * theta);
+p = (h >= .5);
+
+
+
+
 
 % =========================================================================
 
--- sigmoid.m
+++ sigmoid.m
@@ -1,6 +1,19 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid functoon
-%   J = SIGMOID(z) computes the sigmoid of z.
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
 
-g = 1.0 ./ (1.0 + exp(-z));
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+e = exp(1);
+g(:) = 1./(1+e.^(-z));
+
+
+
+
+% =============================================================
+
 end
